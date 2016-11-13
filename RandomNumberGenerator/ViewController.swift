e29ab8a703fcc1248b94a31148a0d7329cc0f2e9
gracektay <gktaydesigns@gmail.com>
2016-11-13 15:53:43 -0500

    Elements added to screen. Successfully set up the button to generate a number between 0 and 9.

--- RandomNumberGenerator/ViewController.swift
+++ RandomNumberGenerator/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var resultString: String = "Result: "
+    var convertedNumber: String = ""
+    
+    //Button click activates number generator
+    @IBAction func generateButton(_ sender: AnyObject) {
+        convertedNumber = String(arc4random() % 10)
+        resultsLabel.text = resultString + convertedNumber
+    }
+    
+    
+    // IBOutlets
+    @IBOutlet var lowerBoundTextField: UITextField!
+    @IBOutlet var upperBoundTextField: UITextField!
+    @IBOutlet var resultsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
